6ef54c00ac77a9752185caffcca540ea6b701104
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2017-10-05 10:08:10 +0200

    - Update EXT JS

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-geoexplorer',
-    version='4.0.14',
+    version='4.0.15',
     author='Ariel Nunez',
     author_email='ingenieroariel@gmail.com',
     url='https://github.com/GeoNode/django-geoexplorer',
