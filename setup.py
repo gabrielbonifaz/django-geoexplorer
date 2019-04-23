b3ec04aac189668f8a6aa2f6559ebbbf48fba6ef
capooti <pcorti@gmail.com>
2019-04-23 15:03:57 -0400

    Bump django-geoexplorer-worldmap 4.0.67

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-geoexplorer-worldmap',
-    version='4.0.64',
+    version='4.0.67',
     author='Matt Bertrand, Paolo Corti',
     author_email='pcorti@gmail.com',
     url='https://github.com/GeoNode/django-geoexplorer/tree/worldmap',
