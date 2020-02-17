a8c1cadc073b5a657488c888a14c7c3c06bfd93d
gabrielbonifaz <gabriel.bonifaz93@gmail.com>
2020-02-17 12:16:36 -0500

    añadir SnappingAgent

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-geoexplorer',
-    version='4.0.43',
-    author='Alessio Fabiani',
-    author_email='alessio.fabiani@gmail.com',
-    url='https://github.com/GeoNode/django-geoexplorer',
+    version='4.0.50',
+    author='Gabriel Bonifaz',
+    author_email='gabriel.bonifaz93@gmail.com',
+    url='https://github.com/gabrielbonifaz/django-geoexplorer',
     download_url = "http://pypi.python.org/pypi/django-geoexplorer/",
     description="Use GeoExplorer in your django projects",
     long_description=open(os.path.join(here, 'README.md')).read(),
